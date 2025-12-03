92834de6ad50dc572a1233e7a36f3499e0711377
d0nghye0n <c20537@kma.ac.kr>
2025-12-04 00:44:25 +0900

    Add Google OAuth client ID and secret to config
    Added Google OAuth configuration parameters.

--- sample/config.py
+++ sample/config.py
@@ -3,6 +3,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    GOOGLE_CLIENT_ID = "861514102522-c8vppikutcunjkj8qshqp2f5c76n926s.apps.googleusercontent.com"
+    GOOGLE_CLIENT_SECRET = "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"
+    GOOGLE_DISCOVERY_URL = ("https://accounts.google.com/.well-known/openid-configuration")
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
